3139d813180349fea24da12031aa6ac6c4e883e1
Pramuditha Gunaratne <pramuditha.gunaratne@infor.com>
2023-10-02 10:27:24 +0530

    schedule list fixes

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ let result = document.getElementById("result");
 let searchBtn = document.getElementById("search-btn");
 let cityRef = document.getElementById("city");
 
-
-
 var dateEl = document.getElementById('date');
 var timeEl = document.getElementById('time');
 
@@ -12,15 +10,12 @@ let append = document.getElementById("search-btn")
 let removeLast = document.querySelector('.removeLast')
 let list = document.querySelector('ul')
 
- 
 let getWeather = () => {
-  let cityValue = cityRef.value;
-  let dateValue  = dateEl.value;
-  let timeValue  = timeEl.value;
+let cityValue = cityRef.value;
+let dateValue  = dateEl.value;
+let timeValue  = timeEl.value;
   
- 
- 
-  if (cityValue.length == 0) {
+if (cityValue.length == 0) {
     result.innerHTML = `<h3 class="msg">Please enter a city name</h3>`;
   }
  
@@ -37,12 +32,11 @@ let getWeather = () => {
         if(dateValue == 0 && timeValue == 0 ){
 
           result.innerHTML = `
-    <div class="result-container">
+        <div class="result-container">
         <div>
         <h2>${data.name}</h2>
         <img src=https://openweathermap.org/img/w/${data.weather[0].icon}.png>
         <h4 class="desc">${data.weather[0].description}</h4>
-        
         </div>
  
         <div class="result-container-second">
@@ -70,8 +64,7 @@ let getWeather = () => {
                 </div>
             </div>
         </div>     
-    </div>
- 
+       </div>
          `;
         }
 
@@ -89,26 +82,18 @@ let getWeather = () => {
             let li = document.createElement('li')
             li.innerHTML = 
             `
-           
-              
                 <h6>${cityValue}</h6>
-                <h7>${dateValue}</h7><h7>${timeValue}</h7>
-             
-          
-          `
+                <h7>${dateValue}</h7><h8>${timeValue}</h8> 
+            `
             return li
           }
           
           
           // Append
           list.append(createLi())
-             
-          
-           
-
-        }
 
         
+        }
 
         })
          
@@ -119,8 +104,8 @@ let getWeather = () => {
   }
 
 
-
 };
+
 
 
 // Remove Last
@@ -129,13 +114,133 @@ removeLast.addEventListener('click', e => {
     let lastNode = list.children[list.children.length - 1]
     console.log(lastNode)
     list.removeChild(lastNode)
+
+    checkTimeEquity()
+     
+
+    
   }
 })  
 
+function checkTimeEquity(){
+  let scheduleList = document.getElementsByTagName("li")
+
+  for (let i = 0; i < scheduleList.length; i++) {
+    
+  
+    let retrieveDate = scheduleList[i].querySelector('h7').innerHTML
+    let retrieveTime = scheduleList[i].querySelector('h8').innerHTML
+    let retrieveCity= scheduleList[i].querySelector('h6').innerHTML
+    let datetime = retrieveDate + " " + retrieveTime + ":00"
+
+    const toTimeStamp = (strDate) => { 
+      const dt = Date.parse(strDate); 
+      return dt / 1000; 
+    }
+    
+    let unixScheduleTime = toTimeStamp(datetime)
+    console.log(unixScheduleTime);
+
+    const date = new Date();
+    const timestamp = Math.floor(date.getTime() / 1000);
+
+    console.log(timestamp);
+
+    if(unixScheduleTime == timestamp){
+
+      getWeatherForSchedule(retrieveCity)
+
+      
+    }
+  }
+
+}
+
+
+
+
+function getWeatherForSchedule(city){
+
+  let retrieveCity = city
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${retrieveCity}&appid=${key}&units=metric`;
+    //Clear
+    
+    fetch(url)
+      .then((resp) => resp.json())
+      //If city name is valid
+      .then((data) => {
+        result.innerHTML = `
+        <div class="result-container">
+        <div>
+        <h2>${data.name}</h2>
+        <img src=https://openweathermap.org/img/w/${data.weather[0].icon}.png>
+        <h4 class="desc">${data.weather[0].description}</h4>
+        </div>
+ 
+        <div class="result-container-second">
+          <h1>${data.main.temp} &#176;</h1>
+            <div class="temp-container">
+                <div>
+                    <h4 class="title">min</h4>
+                    <h4 class="temp">${data.main.temp_min}&#176;</h4>
+                </div>
+                <div>
+                    <h4 class="title">max</h4>
+                    <h4 class="temp">${data.main.temp_max}&#176;</h4>
+                </div>
+            </div>
+ 
+         
+            <div class="other-data">
+                <div>
+                    <h5 class="title"> Humidity</h5>
+                    <h5 class="hum-pre">${data.main.humidity}</h5>
+                </div>
+                <div>
+                    <h5 class="title">pressure</h5>
+                    <h5 class="hum-pre">${data.main.pressure};</h5>
+                </div>
+            </div>
+        </div>     
+       </div>
+         `;
+      })
+
+
+}
+
+
+ 
+let isPaused = false;
+var interval;
+
+function startInterval() {
+  interval = setInterval(checkTimeEquity , 1000);
+}
+
+// Start the interval when the page loads
+startInterval();
+
+// Pause the interval when typing in the input field
+document.getElementById("city").addEventListener("input", function() {
+  if (!isPaused) {
+    clearInterval(interval);
+    isPaused = true;
+    console.log("Paused...");
+  }
+});
+
+// Resume the interval when the button is clicked
+document.getElementById("search-btn").addEventListener("click", function() {
+  if (isPaused) {
+    startInterval();
+    isPaused = false;
+    console.log("Resumed...");
+  }
+});
 
 
 
 
 searchBtn.addEventListener("click", getWeather);
 window.addEventListener("load", getWeather);
- 
